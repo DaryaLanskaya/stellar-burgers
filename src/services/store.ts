1f6c65959c398aadd075f1f3cab2918ae21d6259
Darya Lanskaya <darya.s.lanskaya@yandex.ru>
2025-07-29 15:31:23 +0300

    Add: constructorSlice.ts

--- src/services/store.ts
+++ src/services/store.ts
@@ -9,13 +9,14 @@ import {
 import { ingredientsSliceReducer } from '../slices/ingredientSlice/ingredientSlice'; // Слайс ингредиента
 import { feedSliceReducer } from '../slices/feedSlice/feedSlice'; // Слайс ленты заказов
 import { authSliceReducer } from '../slices/authSlice/authSlice'; // Слайс аутентификации
+import { orderSliceReducer } from '../slices/orderSlice/orderSlice'; // Слайс оформления заказов
 
 // Все редьюсеры объединяем в корневой
 const rootReducer = combineReducers({
   auth: authSliceReducer, // Слайс аутентификации
   ingredients: ingredientsSliceReducer, // Слайс ингредиента
   feeds: feedSliceReducer, // Слайс ленты заказов
-  orders: 
+  orders: orderSliceReducer // Слайс оформления заказов
 });
 
 const store = configureStore({
